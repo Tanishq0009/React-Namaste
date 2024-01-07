ef6e2ee7602c44fdc67245218f4ce2d9d861f92d
Tanishq0009 <tanishqgore69@gmail.com>
2024-01-07 17:25:02 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,9 @@
+const parent=React.createElement("div",{id:"parent"})
+
+
+
+
+const heading= React.createElement("h1",{},"Hello World from react");
+
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
